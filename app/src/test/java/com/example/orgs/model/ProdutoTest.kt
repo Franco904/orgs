ae620bd6e934ba24757dd5af04ade77f48c1b14b
francostavares <francostavares2003@gmail.com>
2023-02-20 16:53:56 -0300

    test: add model dir unit tests + Kluent dependency

---
+++ app/src/test/java/com/example/orgs/model/ProdutoTest.kt
@@ -0,0 +1,70 @@
+package com.example.orgs.model
+
+import org.amshove.kluent.*
+import org.junit.Test
+import java.math.BigDecimal
+
+class ProdutoTest {
+    private lateinit var sut: Produto
+
+    @Test
+    fun `Deve criar uma instancia de Produto com os atributos corretos quando inicializada`() {
+        sut = Produto(
+            titulo = "Alcátra bovina",
+            descricao = "Uma carne macia para um bom almoço",
+            valor = BigDecimal(37.99),
+        )
+
+        sut.shouldBeInstanceOf<Produto>()
+        sut.id.shouldBeEqualTo(0L)
+        sut.titulo.shouldBeEqualTo("Alcátra bovina")
+        sut.descricao.shouldBeEqualTo("Uma carne macia para um bom almoço")
+        sut.valor.shouldBeEqualTo(BigDecimal(37.99))
+        sut.imagemUrl.shouldBeNull()
+        sut.usuarioId.shouldBeEqualTo(0L)
+    }
+
+    @Test
+    fun `Deve criar uma instancia valida de Produto para valores entre 1 e 100`() {
+        sut = Produto(
+            titulo = "Cesta de frutas",
+            descricao = "Uma cesta de frutas para um lanche da tarde",
+            valor = BigDecimal(17.99),
+        )
+
+        sut.isValid.shouldBeTrue()
+    }
+
+    @Test
+    fun `Deve criar uma instancia invalida de Produto para valores iguais a 0`() {
+        sut = Produto(
+            titulo = "Cesta de frutas",
+            descricao = "Uma cesta de frutas para um lanche da tarde",
+            valor = BigDecimal(0.0),
+        )
+
+        sut.isValid.shouldBeFalse()
+    }
+
+    @Test
+    fun `Deve criar uma instancia invalida de Produto para valores menores que 0`() {
+        sut = Produto(
+            titulo = "Cesta de frutas",
+            descricao = "Uma cesta de frutas para um lanche da tarde",
+            valor = BigDecimal(-9),
+        )
+
+        sut.isValid.shouldBeFalse()
+    }
+
+    @Test
+    fun `Deve criar uma instancia invalida de Produto para valores maiores que 100`() {
+        sut = Produto(
+            titulo = "Cesta de frutas",
+            descricao = "Uma cesta de frutas para um lanche da tarde",
+            valor = BigDecimal(120.99),
+        )
+
+        sut.isValid.shouldBeFalse()
+    }
+}
---
+++ app/src/test/java/com/example/orgs/model/UsuarioTest.kt
@@ -0,0 +1,59 @@
+package com.example.orgs.model
+
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeInstanceOf
+import org.amshove.kluent.shouldBeTrue
+import org.junit.Test
+
+class UsuarioTest {
+    private lateinit var sut: Usuario
+
+    @Test
+    fun `Deve criar uma instancia de Usuario com os atributos corretos quando inicializada`() {
+        sut = Usuario(
+            usuario = "francostavares2003@gmail.com",
+            nome = "Franco Saravia Tavares",
+            senha = "123123as",
+        )
+
+        sut.shouldBeInstanceOf<Usuario>()
+        sut.id.shouldBeEqualTo(0L)
+        sut.usuario.shouldBeEqualTo("francostavares2003@gmail.com")
+        sut.nome.shouldBeEqualTo("Franco Saravia Tavares")
+        sut.senha.shouldBeEqualTo("123123as")
+    }
+
+    @Test
+    fun `Deve criar uma instancia valida de Usuario para valores de email e senha validos`() {
+        sut = Usuario(
+            usuario = "francostavares2003@gmail.com",
+            nome = "Franco Saravia Tavares",
+            senha = "123123as",
+        )
+
+        sut.isValid.shouldBeTrue()
+    }
+
+    @Test
+    fun `Deve criar uma instancia invalida de Usuario para valores de email invalidos`() {
+        sut = Usuario(
+            usuario = "francogmail.com",
+            nome = "Franco Saravia Tavares",
+            senha = "123123as",
+        )
+
+        sut.isValid.shouldBeFalse()
+    }
+
+    @Test
+    fun `Deve criar uma instancia invalida de Usuario para valores de senha inferiores a 6 caracteres`() {
+        sut = Usuario(
+            usuario = "francostavares2003@gmail.com",
+            nome = "Franco Saravia Tavares",
+            senha = "one",
+        )
+
+        sut.isValid.shouldBeFalse()
+    }
+}
