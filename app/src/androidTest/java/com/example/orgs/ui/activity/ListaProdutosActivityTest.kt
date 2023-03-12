b625a129ebf33e2432ac5d3f32050ffd453e581c
francostavares <francostavares2003@gmail.com>
2023-03-12 18:39:52 -0300

    fix: fix android test instrumentation set up

--- app/src/androidTest/java/com/example/orgs/ui/activity/ListaProdutosActivityTest.kt
+++ app/src/androidTest/java/com/example/orgs/ui/activity/ListaProdutosActivityTest.kt
@@ -4,11 +4,8 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class ListaProdutosActivityTest {
     @Test
     fun mustDisplayCorrectActivityNameInAppbar() {
